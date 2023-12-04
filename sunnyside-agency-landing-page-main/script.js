4660340906170853e74f98b674471a680a139ca6
Isabella Pabon <132651573+isabellapabons@users.noreply.github.com>
2023-12-04 15:54:54 +0800

    Add files via upload

---
+++ sunnyside-agency-landing-page-main/script.js
@@ -0,0 +1,8 @@
+/* Navigation Bar*/
+
+const hamburger = document.querySelector('.hamburger-menu'); 
+const navMenu = document.querySelector('.nav-menu'); 
+  
+hamburger.addEventListener('click', () => { 
+  navMenu.classList.toggle('hide'); 
+});
